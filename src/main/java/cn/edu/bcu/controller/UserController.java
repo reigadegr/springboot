f4b1fef7114cfeb02c504f91381168d910230181
reigadegr <2722688642@qq.com>
2023-12-05 15:21:21 +0800

    init

---
+++ src/main/java/cn/edu/bcu/controller/UserController.java
@@ -0,0 +1,13 @@
+package cn.edu.bcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping(value="/showlogin")
+    public String showLoginView(){
+
+        return "login";
+    }
+}
