7f5d5d00f9c89192a03332dff1ac4696dda22ced
reigadegr <2722688642@qq.com>
2023-12-12 15:20:36 +0800

    add file Tuser.java

---
+++ src/main/java/cn/edu/bcu/eshop/domain/Tuesr.java
@@ -0,0 +1,50 @@
+package cn.edu.bcu.eshop.domain;
+
+public class Tuesr {
+    private Integer id;
+    private String user_name;
+    private String psw;
+    private String phone;
+    private String address;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
