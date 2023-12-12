ccfcefb722b907e3462ac4b1b3756018817f8f27
reigadegr <2722688642@qq.com>
2023-12-12 14:10:18 +0800

    controller的修改

--- src/main/java/cn/edu/bcu/controller/UserResources.java
+++ src/main/java/cn/edu/bcu/controller/UserResources.java
@@ -2,11 +2,22 @@ package cn.edu.bcu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+
+@RestController
 public class UserResources {
-    @GetMapping(value = "/showlogin") public String showLoginView()
-    {
-        return "login";
+    @PostMapping(value="/api/vi/users")
+    public HashMap<String,Object> register(){
+        HashMap<String,Object> resultMap=new HashMap<>();
+        resultMap.put("code",1000);
+        resultMap.put("msg","注册成功");
+        return resultMap;
     }
+//    @GetMapping(value = "/showlogin") public String showLoginView()
+//    {
+//        return "login";
+//    }
 }
