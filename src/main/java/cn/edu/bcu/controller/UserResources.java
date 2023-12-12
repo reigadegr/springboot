b9c29afc90d568ee43b3cbd219e2972bd1153fef
reigadegr <2722688642@qq.com>
2023-12-12 14:55:04 +0800

    使用ResponseEntity

--- src/main/java/cn/edu/bcu/controller/UserResources.java
+++ src/main/java/cn/edu/bcu/controller/UserResources.java
@@ -1,5 +1,7 @@
 package cn.edu.bcu.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +12,11 @@ import java.util.HashMap;
 @RestController
 public class UserResources {
     @PostMapping(value="/api/vi/users")
-    public HashMap<String,Object> register(){
+    public ResponseEntity<HashMap> register(){
         HashMap<String,Object> resultMap=new HashMap<>();
         resultMap.put("code",1000);
         resultMap.put("msg","注册成功");
-        return resultMap;
+        return new ResponseEntity<>(resultMap, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 //    @GetMapping(value = "/showlogin") public String showLoginView()
 //    {
