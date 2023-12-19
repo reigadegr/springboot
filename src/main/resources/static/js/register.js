8bd982f5f7ecc5540c1d80fb9a4dd305bb32d999
reigadegr <2722688642@qq.com>
2023-12-19 15:01:18 +0800

    提交

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -3,6 +3,24 @@ function register(){
     let psw = $("#psw").val();
     let address = $("#address").val();
     let phone = $("#phone").val();
-
     alert(userName+psw+address+phone);
+
+
+    $.ajax({
+        type:"post",
+        dataType:"json",
+        url:"http//localhost:8082/springboot/api/v1/users",
+        data:{
+            "userName":userName,
+            "psw":psw,
+            "address":address,
+            "phone":phone
+        },
+        success:function (data){
+            alert(data.msg);
+        },
+        error:function (error){
+            alert(error.msg);
+        }
+    });
 }
