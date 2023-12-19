93d353a5e88ce67ea90d4b5eb9c9b5241e924d2a
reigadegr <2722688642@qq.com>
2023-12-19 14:34:08 +0800

    修复无法运行的bug

--- src/main/java/cn/edu/bcu/eshop/mapper/UserMapper.java
+++ src/main/java/cn/edu/bcu/eshop/mapper/UserMapper.java
@@ -4,8 +4,10 @@ package cn.edu.bcu.eshop.mapper;
 import cn.edu.bcu.eshop.domain.Tuser;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 
 @Mapper
+@MapperScan("cn.edu.bcu.eshop.mapper")
 public interface UserMapper {
 
 @Insert("insert into t_user(user_name,psw,phone,address) values("+"#{userName},#{psw},#{phone},#{address})")
--- src/main/java/cn/edu/bcu/eshop/service/UserService.java
+++ src/main/java/cn/edu/bcu/eshop/service/UserService.java
@@ -3,17 +3,21 @@ package cn.edu.bcu.eshop.service;
 
 import cn.edu.bcu.eshop.domain.Tuser;
 import cn.edu.bcu.eshop.mapper.UserMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@MapperScan("cn.edu.bcu.eshop.mapper")
 public class UserService {
     private final UserMapper userMapper;
+
     @Autowired
-    public UserService( UserMapper userMapper){
-        this.userMapper=userMapper;
+    public UserService(UserMapper userMapper) {
+        this.userMapper = userMapper;
     }
-    public int register(Tuser tUser){
+
+    public int register(Tuser tUser) {
         //return 1;
         return this.userMapper.addOne(tUser);
     }
