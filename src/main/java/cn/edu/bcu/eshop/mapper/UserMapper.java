0df7cb07360419edae27fdd3a9060ea3319be427
reigadegr <2722688642@qq.com>
2023-12-19 14:35:28 +0800

    删除无意义注释

--- src/main/java/cn/edu/bcu/eshop/mapper/UserMapper.java
+++ src/main/java/cn/edu/bcu/eshop/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 
 @Mapper
-@MapperScan("cn.edu.bcu.eshop.mapper")
+//@MapperScan("cn.edu.bcu.eshop.mapper")
 public interface UserMapper {
 
 @Insert("insert into t_user(user_name,psw,phone,address) values("+"#{userName},#{psw},#{phone},#{address})")
